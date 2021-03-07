6f021bdaaa777133e8df07037d3d9439e6b6c399
gaidap <pc.gaida@gmail.com>
2021-03-07 17:39:47 +0100

    understanding stephens plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,19 +1,26 @@
 // Copied from Stephen Grinder (Udemy Teacher)
+// Plugin to intercept and override part of esbuild bundling process to prevent access to file system for loading resources.
 import * as esbuild from 'esbuild-wasm';
  
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // intercept bundling process by register to an event listener
+      // and hook into the resolve process.
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
+        console.log('onResolve', args);
+        // add custom namespace to filter encapsulate imported code to prevent conflicts.
         return { path: args.path, namespace: 'a' };
       });
  
+      // Hook into the loading process while bundling, with very simple example.
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
  
+        // Beware, hardcoded imaginary files below.
         if (args.path === 'index.js') {
+          // if esbuild get instruction to look up for something called index.js return stuff below.
           return {
             loader: 'jsx',
             contents: `
