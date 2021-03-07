1bf91d7d36db2627ea07bbc113c4c4229f5a11dd
gaidap <pc.gaida@gmail.com>
2021-03-07 16:36:44 +0100

    initialized esbuild.wasm and using refs to get reference to the wasm service

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const ref = useRef<any>(); // reference hook to get a reference to any component
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []); // Add an empty Array as 2nd argument to signal useEffect to run only once.
 
   const onChangeTA = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(e.target.value);
   };
 
   const onClickSubmit = () => {
-    console.log(input);
+    if   (!ref.current) { // Do nothing if service not ready
+      return;
+    }
+    console.log(ref.current);
   };
 
   return (
@@ -22,7 +38,7 @@ const App = () => {
       <pre>{code}</pre>
     </div>
   );
-};
+};;;;;;;;
 
 ReactDOM.render(
   <App/>, document.querySelector('#root')
