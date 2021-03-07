c145c403c29784ea3bc9c23a83834707d3e7c67a
gaidap <pc.gaida@gmail.com>
2021-03-07 21:53:39 +0100

    first approach to handle relative path on imports

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -13,9 +13,15 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if(args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') { 
-          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' };
-        }
+        } 
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        };
+        // else if (args.path === 'tiny-test-pkg') { 
+        //   return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' };
+        // }
       });
  
       // Hook into the loading process while bundling, with very simple example.
@@ -28,7 +34,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message =  require('tiny-test-pkg');
+              const message =  require('medium-test-pkg');
               console.log(message);
             `,
           };
