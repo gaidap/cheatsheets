2a3d69aeb20d56061a9a9175822aa8ee56a4c05f
gaidap <pc.gaida@gmail.com>
2021-03-08 23:58:02 +0100

    found a way to import css into the esbuild process

--- src/plugins/fetchpkg-plugin.ts
+++ src/plugins/fetchpkg-plugin.ts
@@ -18,14 +18,28 @@ return {
       };
     }
 
-    const cacheHit = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-    if (cacheHit) {
-      return cacheHit;
-    }
+    // const cacheHit = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+    // if (cacheHit) {
+    //   return cacheHit;
+    // }
+    // import 'bulma/css/bulma.css'
     const {data, request} = await axios.get(args.path);     
+    const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+    // Little hack to pipe imported css into a head tag to load it onto the screen
+    const escaped = data
+    .replace(/\n/g, '')
+    .replace(/"/g, '\\"')
+    .replace(/'/g, "\\'");
+    const content = fileType === 'css' ? 
+    `
+    const style = document.createElement('style');
+    style.innerText = '${escaped}';
+    document.head.appendChild(style);
+    ` : data;
     const result: esbuild.OnLoadResult | null = {
       loader: 'jsx',
-      contents: data,
+      contents: content,
       // extract base dir for imported file, e.g. http://.../test-pkg/src/index.js -> /test-pkg/src
       resolveDir: new URL('./', request.responseURL).pathname  
     };
