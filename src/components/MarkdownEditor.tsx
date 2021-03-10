946bf249badfd422964a3575e2a9e5eef58cbe16
gaidap <pc.gaida@gmail.com>
2021-03-10 19:05:40 +0100

    installed markdown editor and created component

---
+++ src/components/MarkdownEditor.tsx
@@ -0,0 +1,8 @@
+import MDEditor from '@uiw/react-md-editor';
+import React from 'react';
+
+const TextEditor: React.FC = () => {
+  return <div></div>;
+};
+
+export default TextEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import CodeCell from './components/CodeCell';
+import MarkdownEditor from './components/MarkdownEditor';
 
 const App = () => {
   return (
     <div>
-       <CodeCell  />
+      <MarkdownEditor />
     </div>
   );
 };
