abd9908fd2cdd2495f5f279149b91a1cc92fbfab
gaidap <pc.gaida@gmail.com>
2021-03-10 17:31:27 +0100

    fixed warning for  useEffect hook

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -35,7 +35,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     return () => {
       window.removeEventListener('resize', listener);
     };
-  }, []);
+  }, [width]);
 
   let resizableProps: ResizableBoxProps;
   if (direction === 'horizontal') {
