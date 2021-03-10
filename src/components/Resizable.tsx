2973c53862a54352faa271a81eae11f4ec5e66db
gaidap <pc.gaida@gmail.com>
2021-03-10 14:24:46 +0100

    improved resizing and handling browser resize

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -1,15 +1,20 @@
 import './Resizable.css';
+import { useState, useEffect } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
 }
 
-const createResizableProps = (direction: string): ResizableBoxProps => {
-  const fivePercentOfWindowHeight = window.innerHeight * 0.05;
-  const ninetyPercentOfWindowHeight = window.innerHeight * 0.9;
-  const twentyPercentOfWindowWidth = window.innerWidth * 0.2;
-  const seventyFivePercentOfWindowWidth = window.innerWidth * 0.75;
+const createResizableProps = (
+  direction: string,
+  innerHeight: number,
+  innerWidth: number
+): ResizableBoxProps => {
+  const fivePercentOfWindowHeight = innerHeight * 0.05;
+  const ninetyPercentOfWindowHeight = innerHeight * 0.9;
+  const twentyPercentOfWindowWidth = innerWidth * 0.2;
+  const seventyFivePercentOfWindowWidth = innerWidth * 0.75;
 
   let result: ResizableBoxProps;
   if (direction === 'horizontal') {
@@ -35,7 +40,27 @@ const createResizableProps = (direction: string): ResizableBoxProps => {
 };
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  const resizableProps = createResizableProps(direction);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const listener = () => {
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', listener);
+
+    // return clean up funcion to deregister from event after work is done
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
+  const resizableProps = createResizableProps(
+    direction,
+    innerHeight,
+    innerWidth
+  );
   return (
     // Infinity for width works analog to "100%"
     <ResizableBox {...resizableProps}>{children}</ResizableBox>
