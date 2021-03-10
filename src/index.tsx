f44e345a7974d9a1457cd46ed84291ce8011e412
gaidap <pc.gaida@gmail.com>
2021-03-10 22:03:09 +0100

    assambled redux store stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
+import CodeCell from './components/CodeCell';
 import MarkdownEditor from './components/MarkdownEditor';
 
 const App = () => {
   return (
     <div>
       <MarkdownEditor />
+      <CodeCell />
     </div>
   );
 };
