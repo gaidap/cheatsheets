cd993abd6f3f4064a643418e03ae1a38b9cd4d4b
gaidap <pc.gaida@gmail.com>
2021-03-10 12:10:03 +0100

    added resize constraints

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -6,9 +6,16 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  const ninetyPercentOfWindowHeight = window.innerHeight * 0.9;
   return (
     // Infinity for width works analog to "100%"
-    <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
+    <ResizableBox
+      maxConstraints={[Infinity, ninetyPercentOfWindowHeight]}
+      minConstraints={[Infinity, 35]}
+      height={300}
+      width={Infinity}
+      resizeHandles={['s']}
+    >
       {children}
     </ResizableBox>
   );
