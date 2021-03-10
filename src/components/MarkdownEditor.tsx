32d4393ad12583826f0d8d1ba431624a395afd01
gaidap <pc.gaida@gmail.com>
2021-03-10 19:37:31 +0100

    toggling the edit and view mode

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -1,8 +1,50 @@
+import { useEffect, useState, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
-import React from 'react';
 
 const TextEditor: React.FC = () => {
-  return <div></div>;
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const clickListener = (event: MouseEvent) => {
+      const clickInsideEditor =
+        divRef.current &&
+        event.target &&
+        divRef.current.contains(event.target as Node);
+
+      if (clickInsideEditor) {
+        return;
+      }
+
+      setEditing(false);
+    };
+    document.addEventListener('click', clickListener, { capture: true });
+
+    const keyListener = (event: KeyboardEvent) => {
+      if (event.code === 'Escape') {
+        setEditing(false);
+      }
+    };
+    document.addEventListener('keyup', keyListener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', clickListener, { capture: true });
+      document.removeEventListener('keyup', keyListener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={divRef}>
+        <MDEditor />
+      </div>
+    );
+  }
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Start writing your Markdown'} />
+    </div>
+  );
 };
 
 export default TextEditor;
