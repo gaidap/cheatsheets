cb63cec118b9b18688afd5be470032f869b4701c
gaidap <pc.gaida@gmail.com>
2021-03-11 18:09:42 +0100

    fixed infinite loop with useMemo added styled progress bar

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,25 +1,28 @@
-import { useState, useEffect } from 'react';
+import './CodeCell.css';
+import { useEffect } from 'react';
 import CodeEditor from './CodeEditor';
 import Preview from './Preview';
 import Resizable, { Direction } from './Resizable';
-import bundle from '../bundler';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 interface CodeCellProps {
   cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState('');
-  const [bundlingStatus, setBundlingStatus] = useState('');
-  const {  updateCell  } = useActions();
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if  (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
-      const bundleOutput = await bundle(cell.content);
-      setCode(bundleOutput.code);
-      setBundlingStatus(bundleOutput.error);
+      createBundle(cell.id, cell.content);
     }, 1000);
 
     return () => {
@@ -27,15 +30,30 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         clearTimeout(timer);
       }
     };
-  }, [cell.content]);
+    // disabled warning: adding bundle will lead to an infinite loop so it cannot be placed in the array 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell.id, cell.content, createBundle]);
 
   return (
     <Resizable direction={Direction.VERTICAL}>
       <div style={{ height: 'calc(100% - 10px)', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction={Direction.HORIZONTAL}>
-          <CodeEditor initialValue={cell.content || '// Start writing some code and have fun!'} onChange={(value) => updateCell(cell.id, value)} />
+          <CodeEditor
+            initialValue={cell.content || '// Start writing some code and have fun!'}
+            onChange={(value) => updateCell(cell.id, value)}
+          />
         </Resizable>
-        <Preview code={code} bundlingStatus={bundlingStatus} />
+        <div className='progress-wrapper'>
+          { !bundle || bundle.loading ? (
+              <div className='progress-bar'>
+                <progress className='progress is-small is-primary' max='100'>
+                  Loading
+                </progress>
+              </div>     
+          ) : (
+            <Preview code={bundle.code} bundlingStatus={bundle.error} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
