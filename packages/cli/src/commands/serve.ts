f3f6f1fe88149e6441ec9269adbab0c8589ae482
gaidap <pc.gaida@gmail.com>
2021-03-12 12:50:32 +0100

    added some error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -2,11 +2,27 @@ import path from 'path';
 import { Command, option } from 'commander';
 import { serve } from 'local-api';
 
+enum ServeErrors {
+  EADDRINUSE = 'EADDRINUSE'
+}
+
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Starts server and opens a file for editing. The filename is optional and defaults to notebook.js.')
   .option('-p, --port <number>', 'Port on which server is listening.', '4005')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir); 
+      console.log(
+        `Opened ${filename}. Please navigate to http://localhost:${options.port} use Cheatsheets.`
+        );
+    } catch (error) {
+      if(error.code === ServeErrors.EADDRINUSE) {
+        console.error('Port is in use. Try running on a different port.');
+      } else{
+        console.error('There was an unexpected problem: ', error.message);
+      }
+      process.exit(1);
+    }
   });
