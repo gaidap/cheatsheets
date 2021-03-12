2b9ce7209c67fb56cbdccf367e683e364f37cf4e
gaidap <pc.gaida@gmail.com>
2021-03-12 13:48:32 +0100

    Link the local-client package as static resource to serve the Cheatsheets app with an absolute path

--- packages/local-api/README.md
+++ packages/local-api/README.md
@@ -1,8 +1,8 @@
 # The Cheatsheets local API
-
-
-
-## How to use the local API
+This API works as a facade for our Cheatsheets react application. So it won't be necessary to start the create-react-app process.
+  
+Instead we will will provide a simple API with express to fetch the needed js and html files to use Cheatsheets.  
+This will be much faster and more effective in storage terms.
 
 
 
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,18 +1,23 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.use(
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      ws: true,
-      logLevel: 'silent',
-    })
-  );
+  // Link the local-client package as static resource to serve the Cheatsheets app with an absolute path
+  const localClientPath = require.resolve('local-client/build/index.html');
+  app.use(express.static(path.dirname(localClientPath)));
+
+  // app.use(
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:3000',
+  //     ws: true,
+  //     logLevel: 'silent',
+  //   })
+  // );
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
   });
-};
+};;
