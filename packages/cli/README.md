889878a19127510ff9f6aae628c5f3da6edf33af
gaidap <pc.gaida@gmail.com>
2021-03-12 12:04:53 +0100

    created my first command with commander

--- packages/cli/README.md
+++ packages/cli/README.md
@@ -5,7 +5,9 @@ This CLI is used as a runner for the Cheatsheets app.
 ## How to use the CLI
 To start the Cheatsheets App you can run the following commands: 
  - cheatsheets serve
- - cheatsheets serve --file notes.js
- - cheatsheets serve --file notes.js --port 3050
+ - cheatsheets serve notes.js
+ - cheatsheets serve notes.js --port 3050
+  
+You can run --help for documentation.
 
 
---
+++ packages/cli/src/commands/serve.ts
@@ -0,0 +1,9 @@
+import { Command, option } from 'commander';
+
+export const serveCommand = new Command()
+  .command('serve [filename]')
+  .description('Starts server and opens a file for editing. The filename is optional and defaults to notebook.js.')
+  .option('-p, --port <number>', 'Port on which server is listening.', '4005')
+  .action((filename = 'notebook.js', options) => {
+    console.log('Getting ready to serve a file.', filename, options);
+  });
