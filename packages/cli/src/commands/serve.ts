a33bc54b28903be416d1ecd67f8f2059782ec5c3
gaidap <pc.gaida@gmail.com>
2021-03-12 12:21:38 +0100

    calculating path and filenames correctly

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,12 @@
+import path from 'path';
 import { Command, option } from 'commander';
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Starts server and opens a file for editing. The filename is optional and defaults to notebook.js.')
   .option('-p, --port <number>', 'Port on which server is listening.', '4005')
-  .action((filename = 'notebook.js', options) => {
-    console.log('Getting ready to serve a file.', filename, options);
+  .action((filename = 'notebook.js', options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
